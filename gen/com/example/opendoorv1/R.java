654d674dc82af5d1a106ab861fd89432fa5e34ec
tomorrowknight <alvinthedeveloper@gmail.com>
2014-02-11 17:41:38 +0800

    Commit Day 2 v1

--- gen/com/example/opendoorv1/R.java
+++ gen/com/example/opendoorv1/R.java
@@ -24,9 +24,10 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080007;
+        public static final int action_settings=0x7f080008;
+        public static final int expandableListView1=0x7f080006;
         public static final int loginBtn=0x7f080003;
-        public static final int menu_forgot_password=0x7f080006;
+        public static final int menu_forgot_password=0x7f080007;
         public static final int mobileNumber=0x7f080001;
         public static final int password=0x7f080002;
         public static final int resetPwdBtn=0x7f080004;
@@ -35,27 +36,30 @@ public final class R {
     }
     public static final class layout {
         public static final int activity_login=0x7f030000;
+        public static final int activity_work=0x7f030001;
     }
     public static final class menu {
         public static final int activity_login=0x7f070000;
         public static final int login=0x7f070001;
+        public static final int work=0x7f070002;
     }
     public static final class string {
         public static final int action_settings=0x7f050001;
-        public static final int action_sign_in_register=0x7f050005;
-        public static final int action_sign_in_short=0x7f050006;
+        public static final int action_sign_in_register=0x7f050006;
+        public static final int action_sign_in_short=0x7f050007;
         public static final int app_name=0x7f050000;
-        public static final int error_field_required=0x7f05000c;
-        public static final int error_incorrect_password=0x7f05000b;
-        public static final int error_invalid_email=0x7f050009;
-        public static final int error_invalid_password=0x7f05000a;
+        public static final int error_field_required=0x7f05000d;
+        public static final int error_incorrect_password=0x7f05000c;
+        public static final int error_invalid_email=0x7f05000a;
+        public static final int error_invalid_password=0x7f05000b;
         public static final int hello_world=0x7f050002;
-        public static final int login_progress_signing_in=0x7f050008;
-        public static final int menu_forgot_password=0x7f050007;
+        public static final int login_progress_signing_in=0x7f050009;
+        public static final int menu_forgot_password=0x7f050008;
         /**  Strings related to login 
          */
-        public static final int prompt_email=0x7f050003;
-        public static final int prompt_password=0x7f050004;
+        public static final int prompt_email=0x7f050004;
+        public static final int prompt_password=0x7f050005;
+        public static final int title_activity_work=0x7f050003;
     }
     public static final class style {
         /** 
